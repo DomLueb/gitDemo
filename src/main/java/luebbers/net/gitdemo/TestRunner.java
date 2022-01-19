ca45bbea032d1d2c2582198d3f7ac3a158f390ad
Dominik Lübbers <dominik@epsilonsoft.de>
2022-01-19 16:54:55 +0100

    Initial commit

---
+++ src/main/java/luebbers/net/gitdemo/TestRunner.java
@@ -0,0 +1,13 @@
+package luebbers.net.gitdemo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TestRunner implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("It runs!");
+    }
+}
